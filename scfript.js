373d013ad79f5e7c103920b634ddb87174017b98
abdulaziz@gmail.com <abdulaziz@gmail.com>
2022-06-28 19:00:54 +0500

    qiduruv tizimi

--- scfript.js
+++ scfript.js
@@ -1,137 +1,43 @@
+const form = document.querySelector(".searchForm");
 
-let loginContainer = document.querySelector('.login-container');
-let qrcodeContainer = document.querySelector('.qrcode-container');
-let codeContainer = document.querySelector('.code-container');
-let phoneCode = document.querySelector('.login-part_country');
-let loginLabel = document.querySelector('.login-part_label');
-let otherInput = document.getElementById('other-input');
-let otherInput1 = document.getElementById('other-input1');
-let otherLabel = document.getElementById('other-label');
-let otherLabel1 = document.getElementById('other-label1');
-let nextBtn = document.querySelector('.next-btn');
-let countryIcon = document.getElementById('login-part_icon');
-let qrcode = document.querySelector('.qr-code');
-let phoneNumber = document.querySelector('.phone-number');
-let verifyNumber = document.querySelector('.verifyNumber');
-let editIcon = document.getElementById('edit-icon');
-let chatList = document.querySelector('.chat-list');
+const submitFunction = (event) => {
+    event.preventDefault();
+    let inputValue = document.querySelector(".searchInput").value;
+    query = inputValue.trim()
 
-phoneCode.onmouseover = function () {
-    loginLabel.style.color = 'rgb(51,144,236)';
-    loginLabel.style.transition = 'all 0.2s';
+    getResults(query)
 }
 
-phoneCode.onmouseout = function () {
-    loginLabel.style.color = 'gray';
-    loginLabel.style.transition = 'all 0.2s';
+form.addEventListener("submit", submitFunction);
+
+const getResults = (query) => {
+    const url = `https://uz.m.wikipedia.org/w/api.php?action=query&list=search&prop=info&origin=*&srlimit=30&utf8=&format=json&srsearch=${query}`
+
+    fetch(url)
+        .then((res) => res.json())
+        .then((data) => {
+            console.log(data.query.search)
+            addResults(data.query.search)
+        })
+        .catch((e) => console.error(`Error: ${e}`));
 }
 
-otherInput.onmouseover = function () {
-    otherLabel.style.color = 'rgb(51,144,236)';
-    otherLabel.style.transition = 'all 0.2s';
+const addResults = (sResults) => {
+    const searchResult = document.querySelector(".results");
+    searchResult.innerHTML = "";
+
+    sResults.forEach((result) => {
+        const url = `https://uz.m.wikipedia.org/?curid=${result.pageid}`;
+
+        searchResult.insertAdjacentHTML(
+            "beforeend", // yoki aftebegin
+            `<div class="card">
+                <h3 class="cardTitle">
+                    <a href="${url}" target="_blank">${result.title}</a>
+                </h3>
+                <span class="cardSnippet">${result.snippet}</span>
+                <br><br> <a href="${url}" class="cardLink" target="_blank">Ko'proq o'qing..</a>
+            </div>`
+        )
+    })
 }
-
-otherInput.onmouseout = function () {
-    otherLabel.style.color = 'gray';
-    otherLabel.style.transition = 'all 0.2s';
-}
-
-otherInput.onkeyup = function () {
-    if (otherInput.value.length >= 7) {
-        nextBtn.style.display = 'block';
-    } else {
-        nextBtn.style.display = 'none';
-    }
-}
-
-countryIcon.onclick = function () {
-    this.setAttribute('class', 'fa-solid fa-angle-up');
-}
-
-countryIcon.ondblclick = function () {
-    this.setAttribute('class', 'fa-solid fa-angle-down');
-}
-
-// QR Code Part
-
-qrcode.onclick = function () {
-    loaded(qrcode);
-    setTimeout(() => {
-        loginContainer.style.display = 'none';
-        qrcodeContainer.style.display = 'flex';
-    }, 500);
-}
-
-phoneNumber.onclick = function () {
-    loginContainer.style.display = 'flex';
-    qrcodeContainer.style.display = 'none';
-}
-
-function loaded(content) {
-    let oldName = content.innerText;
-    content.innerText = 'Please Wait...';
-    setTimeout(() => {
-        content.innerHTML = oldName;
-    }, 1000);
-}
-
-// Code Part
-
-nextBtn.onclick = function () {
-    loaded(nextBtn);
-    setTimeout(() => {
-        verifyNumber.innerText = otherInput.value;
-        loginContainer.style.display = 'none';
-        codeContainer.style.display = 'flex';
-    }, 500);
-}
-
-editIcon.onclick = function () {
-    loginContainer.style.display = 'flex';
-    codeContainer.style.display = 'none';
-}
-
-
-otherInput1.onmouseover = function () {
-    otherLabel1.style.color = 'rgb(51,144,236)';
-    otherLabel1.style.transition = 'all 0.2s';
-}
-
-otherInput1.onmouseout = function () {
-    otherLabel1.style.color = 'gray';
-    otherLabel1.style.transition = 'all 0.2s';
-}
-
-otherInput1.oninput = function () {
-    if (this.value.length === 5) {
-        if (this.value === '12345') {
-            location.href = './telegram.html';
-        } else {
-            this.style.borderColor = 'red';
-            otherLabel1.style.color = 'red';
-        }
-    } else {
-        this.style.borderColor = 'rgba(0 0 0 / 0.2)';
-    }
-
-    if (this.value.length >= 5) {
-        this.value = '';
-    }
-}
-
-otherInput.oninput = function () {
-    if (this.value.length >= 15) {
-        this.value = '';
-    }
-}
-
-
-
-
-
-
-
-
-
-
-
